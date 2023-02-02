fda2078cf1bf087e524fc8c245527c3b9dbad10c
Slb59 <sylvie.bricout2@laposte.net>
2023-02-02 15:32:23 +0100

    Add main structure of code

---
+++ chess.py
@@ -0,0 +1,10 @@
+def main():
+    pass
+    # args = ArgParser()
+    # the_parameters = args.read_parameters()
+    # my_app = BooksToScrape(the_parameters)
+    # my_app.scrapping()
+    # print(my_app)
+
+if __name__ == '__main__':
+    main()
