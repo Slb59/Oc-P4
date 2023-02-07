af33b4f55f6ed47e3fd21beb53d9859a62ba466a
Slb59 <sylvie.bricout2@laposte.net>
2023-02-07 15:58:40 +0100

    Add player.py

---
+++ chessmanager/models/player.py
@@ -0,0 +1,16 @@
+class Player:
+    def __init__(self, chess_id, last_name, first_name, birthday, chess_level):
+        self.chess_id = chess_id
+        self.last_name = last_name
+        self.first_name = first_name
+        self.birthday = birthday
+        self.chess_level = chess_level
+
+    def __str__(self):
+        return f'{self.chess_id} : {self.last_name} {self.first_name}'
+
+    def __eq__(self, other):
+        if isinstance(other, Player):
+            return self.chess_id == other.chess_id
+        else:
+            return False
---
+++ tests/test_player.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from chessmanager.models import Player
+
+
+class TestPlayer(TestCase):
+    def test_init(self):
+        player = Player('AB12345', 'Carlsen', 'Magnus', '30/11/1990', 2852)
+        self.assertEqual(player.__str__(), 'AB12345 : Carlsen Magnus')
+
+    def test_equal(self):
+        player1 = Player('AB12345', 'Carlsen', 'Magnus', '30/11/1990', 2852)
+        player2 = Player('AB12345', 'Carlsen', 'Magnus', '30/11/1990', 2852)
+        self.assertEqual(player1 == player2, True)
+
+    def test_not_equal(self):
+        player1 = Player('AB12345', 'Carlsen', 'Magnus', '30/11/1990', 2852)
+        player2 = Player('AB12346', 'Nepomniachtchi', 'Ian', '14/07/1990', 2793)
+        self.assertEqual(player1 == player2, False)
