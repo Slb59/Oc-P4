cc24fe14a4464c07a338935c87337be6ae9ef059
Slb59 <sylvie.bricout2@laposte.net>
2023-02-14 15:50:22 +0100

    Modify chessmanager.run call

--- chess.py
+++ chess.py
@@ -1,12 +1,8 @@
 from chessmanager.controllers import ArgParser
-from chessmanager.controllers import DatabaseLoader
-
+from chessmanager.controllers import ChessManager
 
 if __name__ == '__main__':
     args = ArgParser()
     the_parameters = args.read_parameters()
-
-    database_loader = DatabaseLoader(the_parameters)
-    my_app = database_loader.load_database()
-
+    my_app = ChessManager(the_parameters)
     my_app.run()
