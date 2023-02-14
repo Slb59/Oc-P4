f16b0efe2c2699e64f400c5917c5848ce5bd1f54
Slb59 <sylvie.bricout2@laposte.net>
2023-02-14 15:53:55 +0100

    add docstrings

--- chessmanager/models/player.py
+++ chessmanager/models/player.py
@@ -1,4 +1,5 @@
 class Player:
+    """ Manage the informations of a chess player """
     def __init__(self, chess_id, last_name, first_name, birthday, chess_level, current_score=0):
         self.chess_id = chess_id
         self.last_name = last_name
@@ -11,8 +12,12 @@ class Player:
         return f'{self.last_name} {self.first_name}'
 
     def __eq__(self, other):
+        """ check if too players are equals : the chess_id is equal """
         if isinstance(other, Player):
             return self.chess_id == other.chess_id
         else:
             return False
+
+
+
 
