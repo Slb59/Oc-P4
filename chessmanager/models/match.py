d7b07affe643632bbb7144408802deb0e55fbba4
Slb59 <sylvie.bricout2@laposte.net>
2023-02-14 15:49:34 +0100

    Delete unused files

--- chessmanager/models/match.py
+++
@@ -1,8 +0,0 @@
-class Match:
-    def __init__(self, no_match, player_white, player_black):
-        self.no_match = no_match
-        self.player_white = player_white
-        self.player_black = player_black
-
-    def __str__(self):
-        return f'match {self.no_match}: {self.player_white} - {self.player_black}'
