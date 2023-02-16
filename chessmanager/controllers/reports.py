1f5270f4547d3fb9735bfef136c3ecbbda77a3dc
Slb59 <sylvie.bricout2@laposte.net>
2023-02-16 21:11:43 +0100

    add elements in all_players reporting

--- chessmanager/controllers/reports.py
+++ chessmanager/controllers/reports.py
@@ -1,4 +1,6 @@
 import webbrowser
+import os
+import datetime
 
 
 class ChessManagerReports:
@@ -9,13 +11,49 @@ class ChessManagerReports:
         self.players = players
         self.tournaments = tournaments
         self.filename = output_directory + '/chessmanager_report.html'
+        self.css_filename = output_directory + '/style.css'
+
+    def style_css(self):
+        # if not os.path.exists(self.css_filename):
+
+        code_css = """      
+        body {
+        font-family: 'Raleway', sans-serif;
+        font-size: 1em;
+        background: rgb(242,151,113);
+        background: linear-gradient(150deg, rgba(242,151,113,1) 0%, rgba(241,226,94,1) 30%, rgba(232,242,31,1) 100%);
+        background-size: cover;
+        background-repeat: no-repeat;
+        opacity : 0.6
+        margin: 0;
+        } 
+        h1 {color: blue; }
+        h2 {text-align: center;}
+        h3 {
+        font-size: 1.3em;
+        color: black;
+        }
+        footer {
+        text-align: center;        
+        }
+        """
+        with open(self.css_filename, 'w', encoding='utf8') as f:
+            f.write(code_css)
 
     def html_header(self) -> str:
         return """
-        <html>
+        <!DOCTYPE html>
+        <html lang="fr">
         <head>
-        <title>Title</title>
+            <meta charset="utf-8">
+            <title>Gestion de tournois d'échecs</title>  
+            <link href="style.css" rel="stylesheet">     
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap" rel="stylesheet"> 
+                          
         </head>
+        <body>
         """
 
     def html_footer(self) -> str:
@@ -24,18 +62,47 @@ class ChessManagerReports:
         </html>
         """
 
+    def report_header(self) -> str:
+        return """
+        <header>
+        <h2>
+        <img src="
+        """ + os.getcwd() + '/logo.png' + """
+        ">
+        Gestionnaire de tournois d'échecs</h2>  
+        </header>    
+        """
+
+    def report_footer(self) -> str:
+        now = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+        return """
+        <footer>
+            <h3>Extraction des données du """ + now + """
+        </h3>
+        </footer>
+        """
+
+    def open_in_browser(self):
+        webbrowser.open(os.getcwd() + '/' + self.filename)
+
     def all_players_in_alphabetic_order(self):
         """ List the players in alphabetic order
         :return:
         """
-        with open(self.filename, 'w') as f:
+        self.style_css()
+        with open(self.filename, 'w', encoding='utf8') as f:
             f.write(self.html_header())
-            body = """<body>
-            <h2>Welcome To GFG</h2>  
-            <p>Default code has been loaded into the Editor.</p>"""
+            f.write(self.report_header())
+            body = """
+            <main>
+            <h1> Liste des joueurs inscrits dans le club </h1>
+            
+            </main>
+            """
             f.write(body)
+            f.write(self.report_footer())
             f.write(self.html_footer())
-        webbrowser.open(self.filename)
+        self.open_in_browser()
 
     def all_tournaments(self):
         """ List the tournaments record in the chess manager
--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,14 +1,24 @@
 from unittest import TestCase
+from tests import TestInit
+from chessmanager.controllers import Parameters
+from chessmanager.controllers import ChessManager
+from chessmanager.controllers import ChessManagerReports
 
 
-class TestChessManagerReports(TestCase):
-    def test_html_header(self):
-        self.fail()
-
-    def test_html_footer(self):
-        self.fail()
+class TestChessManagerReports(TestCase, TestInit):
 
     def test_all_players_in_alphabetic_order(self):
+        # GIVEN
+        a_chess_manager = ChessManager(Parameters())
+        a_chess_manager.players = self.create_8_players(self)
+        chess_manager_reports = ChessManagerReports(
+            a_chess_manager.players,
+            a_chess_manager.tournaments,
+            a_chess_manager.output_directory
+        )
+        # WHEN
+        chess_manager_reports.all_players_in_alphabetic_order()
+        # THEN
         self.fail()
 
     def test_all_tournaments(self):
