3c4c1cea0d3fd127471ebdf49ce1cb8ad041489d
Slb59 <sylvie.bricout2@laposte.net>
2023-02-16 15:42:10 +0100

    add tests for reports

---
+++ tests/test_reports.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class TestChessManagerReports(TestCase):
+    def test_html_header(self):
+        self.fail()
+
+    def test_html_footer(self):
+        self.fail()
+
+    def test_all_players_in_alphabetic_order(self):
+        self.fail()
+
+    def test_all_tournaments(self):
+        self.fail()
+
+    def test_tournament_data(self):
+        self.fail()
+
+    def test_tournament_players(self):
+        self.fail()
+
+    def test_tournaments_details(self):
+        self.fail()
