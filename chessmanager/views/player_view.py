53fc5e51afceee9d9d8910bfa2c1790baa9ad55c
Slb59 <sylvie.bricout2@laposte.net>
2023-02-21 16:26:38 +0100

    Add test for ELO input

--- chessmanager/views/player_view.py
+++ chessmanager/views/player_view.py
@@ -11,20 +11,22 @@ def prompt_player_id(self) -> str:
         validate=lambda text: True if re.match(r"[A-Z][A-Z]\d\d\d\d\d", text)
         else "L'identifiant doit contenir 2 lettres et 5 chiffres"
     ).ask()
-    # while not re.match(r"[A-Z][A-Z]\d\d\d\d\d", chess_id):
-    #     print("L'identifiant doit contenir 2 lettres et 5 chiffres")
-    #     chess_id = questionary.text("Identifiant national d'échecs:").ask()
     return chess_id
 
 
 def prompt_player_data(self) -> tuple:
     first_name = questionary.text("Nom:").ask()
     last_name = questionary.text("Prénom:").ask()
-    birthday = questionary.text("Date de naissance (dd/mm/yyyy):").ask()
-    while not check_date_format(birthday):
-        print("Format de date incorrect")
-        birthday = questionary.text("Date de naissance (dd/mm/yyyy):").ask()
-    chess_level = questionary.text("Points ELO:").ask()
+    birthday = questionary.text(
+        "Date de naissance (dd/mm/yyyy):",
+        validate=lambda text: True if check_date_format(text)
+        else "Format de date incorrect : dd/mm/yyyy"
+    ).ask()
+    chess_level = questionary.text(
+        "Points ELO:",
+        validate=lambda text: True if len(text) > 0 and (1000 <= int(text) <= 3000)
+        else "Les points ELO se situe entre 1000 et 3000"
+    ).ask()
     return first_name, last_name, birthday, chess_level
 
 class PlayerView:
